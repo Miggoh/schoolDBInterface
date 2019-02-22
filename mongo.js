af51038fe72d4b03c33258281b8fb9fb847563a8
Miggoh <45897621+Miggoh@users.noreply.github.com>
2019-02-22 15:07:49 +0200

    Delete mongo.js
    Removed obsolete script

--- mongo.js
+++
@@ -1,8 +0,0 @@
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://kay:myRealPassword@cluster0.mongodb.net/admin";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
- // perform actions on the collection object
-  client.close();
-});
