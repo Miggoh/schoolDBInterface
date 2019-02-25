67333cd7c067e545a67c55d5780851e300300ebf
Miggoh <45897621+Miggoh@users.noreply.github.com>
2019-02-25 02:21:43 +0200

    Icons
    Added a lot of icons. Made a new partial view template for login data.

--- public/client.js
+++ public/client.js
@@ -6,14 +6,14 @@ swapper.addEventListener('click', function(){ card.innerHTML =
                   <form class="form-signin" method="POST" action="/login">
                     <div class="form-label-group">
                       <input type="text" id="inputUserame" class="form-control" placeholder="Username" autocomplete="username" name="username" required autofocus>
-                      <label for="inputUserame">Username</label>
+                      <label for="inputUserame"><i class="fas fa-user"></i> Username</label>
                     </div>
                     
                     <hr>
       
                     <div class="form-label-group">
                       <input type="password" id="inputPassword" class="form-control" placeholder="Password" autocomplete="current-password" name="password" required>
-                      <label for="inputPassword">Password</label>
+                      <label for="inputPassword"><i class="fas fa-key"></i> Password</label>
                     </div>
-                    <button class="btn btn-lg btn-primary btn-block text-uppercase" type="submit">Login</button>
+                    <button class="btn btn-lg btn-primary btn-block text-uppercase" type="submit"><i class="fas fa-sign-in-alt"></i> Login</button>
                   </form>`});
