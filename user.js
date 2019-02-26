7f6dcc6fd42f17c30798981a3ed8c25d4a4a33aa
Miggoh <45897621+Miggoh@users.noreply.github.com>
2019-02-26 12:34:48 +0200

    Minify
    minified script files for production

--- user.js
+++ user.js
@@ -1,12 +1,1 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const userSchema = new Schema({
-    username: String,
-    password: String,
-    email: String,
-    courses: Array,
-}, { collection: "studentData" });
-const user = mongoose.model('user', userSchema);
-
-module.exports = user;
+const mongoose=require("mongoose"),Schema=mongoose.Schema,userSchema=new Schema({username:String,password:String,email:String,courses:Array},{collection:"studentData"}),user=mongoose.model("user",userSchema);module.exports=user;
